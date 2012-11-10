29bd1d30757524196d8de5fb326e6f2f897c2b66
Ryan Sylvestre <rjsylvestre@gmail.com>
2012-11-09 23:46:31 -0500

    Added gameLookup, gets data from steam pages

---
+++ libs/gameLookup.js
@@ -0,0 +1,88 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+var getGeneres = function($){
+    var results = [];
+    $('.game_details a').each(function(){
+        var $this = $(this);
+         if ($this.attr('href').search('store.steampowered.com/genre/') !== -1) {
+             results.push($this.text());
+         }
+    });
+    return results;
+};
+
+var getName = function($){
+    return $('.page_title_area .apphub_AppName').text();
+};
+
+var getGameIcon = function($){
+    return $('.page_title_area .apphub_AppIcon img').attr('src');
+};
+
+var getMetaScore = function($){
+    return $('#game_area_metascore').text().trim();
+};
+
+var getDetailSpecs = function($){
+    var result = {
+        isMultiplayer: false,
+        isCoop: false
+    };
+    $('.game_area_details_specs').each(function(){
+        $(this).find('.name').each(function(){
+            var text = $(this).text();
+            if (text === 'Multi-player'){
+                result.isMultiplayer = true;
+            }
+            if (text === 'Co-op'){
+                result.isCoop = true;
+            }
+        });
+    });
+    return result;
+};
+
+
+var getGameInfo = function(appId){
+    request.post('http://store.steampowered.com/agecheck/app/' + appId,
+        {
+            form : {
+                snr: '1_agecheck_agecheck__age-gate',
+                ageDay: '1',
+                ageMonth: 'January',
+                ageYear: '1975'
+            }
+        },
+        function(err){
+            var $, specs, results;
+
+            if (err){
+                return { error: 'Something went bad'};
+            }
+
+            request('http://store.steampowered.com/app/' + appId, function(err, data){
+                if (err){
+                    return { error: 'Something went bad'};
+                }
+
+                $ = cheerio.load(data.body);
+                specs = getDetailSpecs($);
+                results = {
+                    name: getName($),
+                    generes: getGeneres($),
+                    isMultiplayer: specs.isMultiplayer,
+                    isCoop: specs.isCoop,
+                    gameIcon: getGameIcon($),
+                    metaScore: getMetaScore($) || null
+                }
+
+                return results;
+            });
+    });
+};
+//getGameInfo(4920);
+//getGameInfo(49520);
+//getGameInfo(218040);
+
+module.exports = getGameInfo;
