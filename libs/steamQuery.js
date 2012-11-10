ba931b11cdc63cd1cfd323667ec77374e28b6650
Will Klein <will.klein@gmail.com>
2012-11-10 16:29:58 -0500

    Add xml2json.

--- libs/steamQuery.js
+++ libs/steamQuery.js
@@ -21,39 +21,54 @@
     };
     
     
-    var player = function(playerKey) {
+    var player = function(playerKey, cb) {
         if (!playerKey.customURL && playerKey.steamID64) {
-            getCustomURL(playerKey.steamID64);
+            playerKey.customURL = getCustomURL(playerKey.steamID64);
         }
         
-//        var apiRequest = request(steamCommunity.);
-        var parser = xml2json.Parser();
+        request(steamCommunity.getProfile(playerKey.customURL), function(err, data) {
+            if (err) {
+                cb({ error: "steamCommunity API Error: " + err });
+            }
+            
+            var parser = xml2json.Parser();
+            
+            parser.parseString(data.body, function(err, result) {
+                if (err) {
+                    cb({ error: "JSON Parser Error: " + err });
+                }
+
+                cb(null, result);
+            });
+        });
         // mock data
-        return {
-            "steamID64": "76561197972886336",
-            "steamID": "WillSci",
-            "customURL": "willscience",
-            "avatarIcon": "http://media.steampowered.com/steamcommunity/public/images/avatars/a6/a6340d219beb553b9679d18a7b229620c59c39ad.jpg",
-            // more fields
-
-            "games": [
-                {
-                    "appID": "8930"
-                },
-                {
-                    "appID": "49520"
-                }
-            ],
-
-            "groups": [
-                {
-                    "groupID64": "103582791429570843"
-                },
-                {
-                    "groupID64": "103582791429729126"
-                }
-            ]
-        };
+//        result = {
+//            "steamID64": "76561197972886336",
+//            "steamID": "WillSci",
+//            "customURL": "willscience",
+//            "avatarIcon": "http://media.steampowered.com/steamcommunity/public/images/avatars/a6/a6340d219beb553b9679d18a7b229620c59c39ad.jpg",
+//            // more fields
+//
+//            "games": [
+//                {
+//                    "appID": "8930"
+//                },
+//                {
+//                    "appID": "49520"
+//                }
+//            ],
+//
+//            "groups": [
+//                {
+//                    "groupID64": "103582791429570843"
+//                },
+//                {
+//                    "groupID64": "103582791429729126"
+//                }
+//            ]
+//        };
+        
+        
     };
     
     var steamQuery = function() {
