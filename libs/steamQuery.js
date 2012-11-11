cf63c26b20437f4d2c9e429ff16f0d13bca8a8e8
Will Klein <will.klein@gmail.com>
2012-11-10 20:14:58 -0500

    Amazing Steam API access for games!

--- libs/steamQuery.js
+++ libs/steamQuery.js
@@ -1,6 +1,7 @@
 var xml2json = require('xml2json');
 var request = require('request');
 var readProfile = require('./readProfile');
+var readGames = require('./readGames');
 
 var steamCommunity = {
     getProfile: function(customURL) {
@@ -12,8 +13,14 @@ var steamCommunity = {
     getGames: function(customURL) {
         return 'http://steamcommunity.com/id/' + customURL + '/games/?xml=1';
     },
+    getGamesByID64: function(steamID64) {
+        return 'http://steamcommunity.com/profiles/' + steamID64 + '/games/?xml=1';
+    },
     getFriends: function(customURL) {
         return 'http://steamcommunity.com/id/' + customURL + '/friends/?xml=1';
+    },
+    getFriendsByID64: function(steamID64) {
+        return 'http://steamcommunity.com/profiles/' + steamID64 + '/friends/?xml=1';
     }
 };
 
@@ -21,7 +28,7 @@ var player = function(playerKey, cb) {
     var url = playerKey.customURL
             ? steamCommunity.getProfile(playerKey.customURL)
             : steamCommunity.getProfileByID64(playerKey.steamID64);
-        
+    
     request(url, function(err, data) {
         if (err) {
             cb({ error: "steamCommunity API Error: " + err });
@@ -34,25 +41,50 @@ var player = function(playerKey, cb) {
     });
 };
 
+var games = function(playerKey, cb) {
+    var url = playerKey.customURL
+        ? steamCommunity.getGames(playerKey.customURL)
+        : steamCommunity.getGamesByID64(playerKey.steamID64);
+
+    request(url, function(err, data) {
+        if (err) {
+            cb({ error: "steamCommunity API Error: " + err });
+        }
+
+        var result = xml2json.toJson(data.body, { object: true });
+        var games = readGames(result);
+
+        cb(null, games);
+    });
+};
+
 var steamQuery = function() {
     return {
-        player: player
+        player: player,
+        games: games
     };
 };
 
 // sample usage
 
-var query = steamQuery();
-
-query.player({
+var playerKey = {
     steamID64: '76561197972886336'
 //  , customURL: 'willscience'
-}, function(err, result) {
-    if (err) {
-        console.log("Error: " + err);
-    }
-    
-    console.log(result);
-});
+};
+var query = steamQuery();
+
+//query.player(playerKey, function(err, result) {
+//    if (err) {
+//        console.log("Error: " + err);
+//    }
+//    console.log(result);
+//});
+
+//query.games(playerKey, function(err, result) {
+//    if (err) {
+//        console.log("Error: " + err);
+//    }
+//    console.log(result);
+//});
 
 module.exports = steamQuery;
